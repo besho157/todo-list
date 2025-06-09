f3b1a9721948c1ff76be25da9a9eb7464b06576d
besho157 <beshomeshel15@gmail.com>
2025-06-09 18:16:03 +0300

    fix: resolve login form validation error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,11 +40,12 @@ export default function Login() {
 
         }
         else {
-            setError(" Incorrect username or password")
+            setError('Incorrect username or password')
         }
 
     }
-    return  (
+    return (
+
         <form onSubmit={handleLogin} className="mx-auto max-w-sm lg:max-w-4xl">
             <div className='flex items-center gap-5'>
                 <div className="w-8 h-8 bg-blue-500 rounded-lg flex items-center justify-center">
@@ -114,7 +115,6 @@ export default function Login() {
                         <button className="bg-blue-600 text-white font-bold py-2 px-4 w-full rounded hover:bg-blue-800">Log in</button>
                         {error && <p className='flex m-auto items-center  gap-4 bg-red-100 border border-red-400 text-red-700 px-4 py-2 mt-4 rounded relative'>{<MdErrorOutline />}{error}</p>}
                     </div>
-
                     <div className="mt-4 flex items-center justify-center">
                         <span>Don't have an account?
                             <Link href="/sign-up" className="text-xs text-blue-800 uppercase"> Sign up</Link>
